6c23fffd44abb2641298888fe1af018c14968fe2
ignjatovicpavle <116836553+ignjatovicpavle@users.noreply.github.com>
2024-10-12 16:20:07 +0200

    Added speed calculation

--- project.py
+++ project.py
@@ -1,8 +1,10 @@
 import pygame as pg
+import time
 
 def main():
 
     pg.init()
+    pg.event.clear()
     width, height = 1000, 600
     background_color = (0, 0, 255)
     text_color = (0,0,0)
@@ -12,8 +14,11 @@ def main():
     pg.display.set_caption("The project")
 
     running = True
+    started = False
+    ended = False
 
     target = "The quick brown fox jumps over the lazy dog"
+    n = len(target)
 
     target_l = [char for char in target]
     userText = []
@@ -22,39 +27,66 @@ def main():
     outputText = ""
 
     while running:
-        screen.fill(background_color)
-        for event in pg.event.get():
+        if len(userText) >= n:
+            ended = True
+
+        if not ended:
+
+            screen.fill(background_color)
+
+            event = pg.event.wait()
             if event.type == pg.QUIT:
-                pg.quit()
+                running = False
             else:
                 if event.type == pg.KEYDOWN:
+
+                    if not started:
+                        started = True
+                        t0 = time.time()
+
                     if event.key != pg.K_BACKSPACE:
                         userText.append(event.unicode)
                     else:
                         if len(userText) > 0:
                             userText = userText[:-1]
 
-        font = pg.font.Font(None, 32)
-        w = 0
+            font = pg.font.Font(None, 32)
+            w = 0
 
-        for i in range (len(userText)):
-            char = userText[i]
-            if char == target[i]:
-                color = correct_color
-            else:
-                color = incorrect_color
+            for i in range (len(userText)):
+                char = userText[i]
+                if char == target[i]:
+                    color = correct_color
+                else:
+                    color = incorrect_color
 
-            t = font.render(char, True, color)
-            screen.blit(t, (w + 20,20))
-            w+=t.get_rect().width
+                t = font.render(char, True, color)
+                screen.blit(t, (w + 20,20))
+                w+=t.get_rect().width
 
-        userText_str = ''.join(userText)
-        text2 = font.render(target[len(userText_str):], True, text_color)
-        
-        
-        screen.blit(text2, (w + 20 ,20))
+            userText_str = ''.join(userText)
+            text2 = font.render(target[len(userText_str):], True, text_color)
+            
+            
+            screen.blit(text2, (w + 20 ,20))
 
-        pg.display.update()
+        else:
+            t1 = time.time()
+            wpm = round((n / 5) / (t1-t0) * 60)
+
+            print(f"Ended. {wpm} WPM")
+            text = font.render(f"Test ended. Result: {wpm} WPM", True, text_color)
+            screen.blit(text, (width // 2 - text.get_width() // 2, height // 2 - text.get_height() // 2 ))
+            running = False
+
+        pg.display.flip()
+
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                break
 
 if __name__ == "__main__":
     main()
